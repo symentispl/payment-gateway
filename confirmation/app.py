7ddecf993431c02dbb20e6ea805c51916b864f1c
palkaszymon <s221437@student.uek.krakow.pl>
2022-10-26 14:25:14 +0200

    Added basic tpay confirmation handling and DynamoDB integration

--- confirmation/app.py
+++ confirmation/app.py
@@ -1,2 +1,17 @@
+import json
+from urllib.parse import parse_qsl
+import boto3
+
 def lambda_handler(event, context=None):
-    pass
+    dynamodb = boto3.resource('dynamodb', region_name='eu-central-1')
+    table = dynamodb.Table('payments')
+    item= dict()
+    for i in parse_qsl(event['body']):
+        item[i[0]] = i[1]
+    if event['requestContext']['identity']['sourceIp'] in ['195.149.229.109', '148.251.96.163', '178.32.201.77', '46.248.167.59', '46.29.19.106', '176.119.38.175'] and item['tr_status'] == 'TRUE':
+        data = table.put_item(Item=item)
+    print(event)
+    response = dict()
+    response["statusCode"] = 200
+    response["body"] = "TRUE"
+    return response
--- payment/app.py
+++ payment/app.py
@@ -4,9 +4,10 @@ import json
 
 def lambda_handler(event, context=None):
     code = 'GqtXpYSEk160KOW9'
+    id=31744
     params = event.get('queryStringParameters')
     amount = params.get('amount')
-    description = params.get('description`')
+    description = params.get('description')
     crc = uuid.uuid4()
     md5string = '&'.join([str(id),str(amount),str(crc),code])
     url = f'https://secure.tpay.com?id=31744&amount={amount}&description={description}&crc={crc}&md5sum={hashlib.md5(md5string.encode("utf-8")).hexdigest()}'
