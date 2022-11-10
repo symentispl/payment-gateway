cc78aa8ca7083bd9c8a7c76e1ffdad67d62d132a
palkaszymon <s221437@student.uek.krakow.pl>
2022-11-10 21:34:06 +0100

    Added discounts handling

--- payment/app.py
+++ payment/app.py
@@ -13,20 +13,29 @@ def encode_description(description):
     return result[3:]
 
 def lambda_handler(event, context=None):
-    code = 'GqtXpYSEk160KOW9'
-    id=31744
+    CODE = 'GqtXpYSEk160KOW9'
+    ID = 31744
     dynamodb = boto3.resource('dynamodb', region_name='eu-central-1')
-    table = dynamodb.Table('courses')
+    courses_table = dynamodb.Table('courses')
+    discounts_table = dynamodb.Table('discounts')
+    
     params = event.get('queryStringParameters')
     course_name = params.get('course_name')
     discount = params.get('discount')
-    amount = table.get_item(Key = {'course_name': course_name})['Item']['amount']
+    
+    amount = courses_table.get_item(Key = {'course_name': course_name})['Item']['amount']
     if type(amount) == int:
         amount = float(amount)
+    if discount:
+        if discounts_table.get_item(Key = {'discount_code': discount})['Item']['is_active'] == True:
+            amount -= amount * (discounts_table.get_item(Key = {'discount_code': discount})['Item']['discount_percentage'])/100
+
     description = encode_description(course_name)
     crc = uuid.uuid4()
-    md5string = '&'.join([str(id),str(amount),str(crc),code])
-    url = f'https://secure.tpay.com?id=31744&amount={amount}&description={description}&crc={crc}&md5sum={hashlib.md5(md5string.encode("utf-8")).hexdigest()}'
+    
+    md5string = '&'.join([str(ID),str(round(amount, 2)),str(crc),CODE])
+    
+    url = f'https://secure.tpay.com?id=31744&amount={round(amount, 2)}&description={description}&crc={crc}&md5sum={hashlib.md5(md5string.encode("utf-8")).hexdigest()}'
     response = dict()
     response["statusCode"] = 303
     response["body"] = json.dumps(dict())
