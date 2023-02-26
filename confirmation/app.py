25845520904f86b70924aba1702db7107bd5d02c
Szymon Pałka <szymon.palka02@gmail.com>
2023-02-26 10:08:31 +0100

    Added courses table integration

--- confirmation/app.py
+++ confirmation/app.py
@@ -8,8 +8,8 @@ def lambda_handler(event, context=None):
     item= dict()
     for i in parse_qsl(event['body']):
         item[i[0]] = i[1]
-    if event['requestContext']['identity']['sourceIp'] in ['195.149.229.109', '148.251.96.163', '178.32.201.77', '46.248.167.59', '46.29.19.106', '176.119.38.175'] and item['tr_status'] == 'TRUE':
-        data = table.put_item(Item=item)
+    if event['requestContext']['identity']['sourceIp'] in ['195.149.229.109', '148.251.96.163', '178.32.201.77', '46.248.167.59', '46.29.19.106', '176.119.38.175']:
+        table.put_item(Item=item)
     print(event)
     response = dict()
     response["statusCode"] = 200
