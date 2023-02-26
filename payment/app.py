f21191c72bba0fc2010a9b42489dc44b5c91ef36
Szymon Pałka <szymon.palka02@gmail.com>
2023-02-26 10:08:31 +0100

    Corrected previous commit, actually updated payment/app.py

--- payment/app.py
+++ payment/app.py
@@ -1,13 +1,29 @@
+import boto3
 import uuid
 import hashlib
 import json
 
+def encode_description(description):
+    result = ""
+    for i in description:
+        if i.isupper():
+            result=result+"%20"+i.upper()
+        else:
+            result=result+i
+    return result[3:]
+
 def lambda_handler(event, context=None):
     code = 'GqtXpYSEk160KOW9'
     id=31744
+    dynamodb = boto3.resource('dynamodb', region_name='eu-central-1')
+    table = dynamodb.Table('courses')
     params = event.get('queryStringParameters')
-    amount = params.get('amount')
-    description = params.get('description')
+    course_name = params.get('course_name')
+    discount = params.get('discount')
+    amount = table.get_item(Key = {'course_name': course_name})['Item']['amount']
+    if type(amount) == int:
+        amount = float(amount)
+    description = encode_description(course_name)
     crc = uuid.uuid4()
     md5string = '&'.join([str(id),str(amount),str(crc),code])
     url = f'https://secure.tpay.com?id=31744&amount={amount}&description={description}&crc={crc}&md5sum={hashlib.md5(md5string.encode("utf-8")).hexdigest()}'
