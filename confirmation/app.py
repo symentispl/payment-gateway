2ae72c763fe74f6fa1f9f50e322d94d422ed3250
Szymon Pałka <szymon.palka02@gmail.com>
2023-02-26 10:10:47 +0100

    added SES SMTP service

--- confirmation/app.py
+++ confirmation/app.py
@@ -1,29 +1,20 @@
-import os, smtplib, ssl, boto3
+import os, smtplib, ssl, boto3,json
 from urllib.parse import parse_qsl
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from mako.template import Template
+from botocore.exceptions import ClientError
+
 
 region = os.environ['AWS_REGION']
 stage_prefix = os.environ['STAGE_PREFIX']
+
+smtp_secret_name = "ses-smtp-user"
 
 dynamodb = boto3.resource('dynamodb', region_name=region)
 payment_table = dynamodb.Table(stage_prefix+'payments')
 courses_table = dynamodb.Table(stage_prefix+'courses')
 
-def send_email(email_to, course_name, amount, date, location):
-    html = Template(filename='mail_template.html', input_encoding='UTF-8').render_unicode(course_name, amount, date, location).encode('UTF-8')
-    email_from = 'sender_email@gmail.com'  #dajesz email symentis chyba
-    password = 'xxx'
-    email_message = MIMEMultipart()
-    email_message['From'], email_message['To'], email_message['Subject'] = email_from, email_to, "Zakup szkolenia Symentis"
-    email_message.attach(MIMEText(html, "html"))
-
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server: #tutaj wpisujesz swoj email provider i port(?)
-        server.login(email_from, password)
-        server.sendmail(email_from, email_to, email_message.as_string())
-    
 def lambda_handler(event, context=None):
     item = dict()
     for i in parse_qsl(event['body']):
@@ -39,3 +30,34 @@ def lambda_handler(event, context=None):
     response["statusCode"] = 200
     response["body"] = "TRUE"
     return response
+
+def send_email(email_to, course_name, amount, date, location):
+    smtp_secret = get_smtp_secret()
+    html = Template(filename='mail_template.html', input_encoding='UTF-8').render_unicode(course_name, amount, date, location).encode('UTF-8')
+    email_from = 'no-replay@payment.jvmperformance.pl'  
+    email_message = MIMEMultipart()
+    email_message['From'], email_message['To'], email_message['Subject'] = email_from, email_to, "Zakup szkolenia Symentis"
+    email_message.attach(MIMEText(html, "html"))
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("email-smtp.eu-central-1.amazonaws.com", 587, context=context) as server: #tutaj wpisujesz swoj email provider i port(?)
+        server.login(smtp_secret["smtp-username"], smtp_secret["smtp-password"])
+        server.sendmail(email_from, email_to, email_message.as_string())
+    
+def get_smtp_secret():
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region
+    )
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=smtp_secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+    # Decrypts secret using the associated KMS key.
+    secret = get_secret_value_response['SecretString']
+    return json.loads(secret)
